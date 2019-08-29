b810cdf1e0f1db8c41e0017b41cef00ac86c4a03
Canberk Aslan <canberk.aslan@aurea.com>
2019-08-29 10:40:20 +0300

    Update cluster_test.go

--- cluster_test.go
+++ cluster_test.go
@@ -9,8 +9,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/go-redis/redis"
-	"github.com/go-redis/redis/internal/hashtag"
+	"github.com/canberkaslan/redis"
+	"github.com/canberkaslan/redis/internal/hashtag"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
