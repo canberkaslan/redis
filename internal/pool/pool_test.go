b71ec7b5739ae049bbe09b3f8ca73c888e40d577
Canberk Aslan <canberk.aslan@aurea.com>
2019-08-29 10:47:16 +0300

    Update pool_test.go

--- internal/pool/pool_test.go
+++ internal/pool/pool_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-redis/redis/internal/pool"
+	"github.com/canberkaslan/redis/internal/pool"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
